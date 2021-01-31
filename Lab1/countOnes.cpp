ff084b56b0dca922966f6fa835b2c799abdd20ab
mingw4 <mingw4@illinois.edu>
2021-01-31 17:32:57 -0600

    extract message remain undone.

--- Lab1/countOnes.cpp
+++ Lab1/countOnes.cpp
@@ -5,6 +5,14 @@
 
 unsigned countOnes(unsigned input) {
 	// TODO: write your code here
+	input = ((input & 0xAAAAAAAA) >> 1) + (input & 0x55555555);
+	input = ((input & 0xCCCCCCCC) >> 2) + (input & 0x33333333);
+	input = ((input & 0xF0F0F0F0) >> 4) + (input & 0xF0F0F0F);
+	input = ((input & 0x00FF00FF) >> 8) + (input & 0xFF00FF00);
+	input = ((input & 0x0000FFFF) >> 16) + (input & 0xFFFF0000);
+	
+
+
 
 	
 
--- Lab1/keypad.v
+++ Lab1/keypad.v
@@ -3,4 +3,15 @@ module keypad(valid, number, a, b, c, d, e, f, g);
    output [3:0] number;
    input 	a, b, c, d, e, f, g;
 
+   wire w1;
+
+   or o1(w1, a, b, c);
+   or o2(valid, (w1 && d), (w1 && e), (w1 && f), (b && g));
+
+   assign number[0] = (((a || c) && (d || f)) || (b && e));
+   assign number[1] = ((d && (b || c)) || (c && e) || (a && f));
+   assign number[2] = ((e && (a || b)) || (c && e) || (a && f));
+   assign number[3] = (f && (b || c));
+
+
 endmodule // keypad
