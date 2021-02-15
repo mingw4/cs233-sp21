0eccebb64b895c15b1360033a318c0434ebacacd
mingw4 <mingw4@illinois.edu>
2021-02-14 21:58:59 -0600

    GROUP MERGE

--- Lab3/arraySortCheck_control.v
+++ Lab3/arraySortCheck_control.v
@@ -4,32 +4,27 @@ module arraySortCheck_control(sorted, done, load_input, load_index, select_index
 	input go, inversion_found, end_of_array, zero_length_array;
 	input clock, reset;
 
-	wire sGarbage, sStart, sProcess, sForward, sYes, sNo;
+	wire garbage, doNothing, check, notSort, sort;
+	//wire garbage_next, doNothing_next, check_next, notSort_next, sort_next;
+	wire garbage_next = reset | garbage & ~go;
+	wire doNothing_next = ~reset & go & (doNothing | garbage | notSort | sort);//? wait for campuswire response
+	wire check_next = ~reset & ((check & ~inversion_found & ~end_of_array & ~zero_length_array) | (doNothing & ~go));
+	wire notSort_next = ~reset & ((check & inversion_found & ~end_of_array & ~zero_length_array) | (notSort & ~go));
+	wire sort_next = ~reset & ((check & ~inversion_found & (end_of_array | zero_length_array)) | (sort & ~go));
 
-	wire sProcess_next = ~reset & (sForward | (sStart & ~go));
-	wire sForward_next = ~reset & ~end_of_array & ~inversion_found & sProcess;
+	dffe toGarbage(garbage, garbage_next, clock, 1'b1, 1'b0);
+	dffe t0DoNothing(doNothing, doNothing_next, clock, 1'b1, 1'b0);
+	dffe toCheck(check, check_next, clock, 1'b1, 1'b0);
+	dffe toNotSort(notSort, notSort_next, clock, 1'b1, 1'b0);
+	dffe toSort(sort, sort_next, clock, 1'b1, 1'b0);
 
-	wire sYes_next = ~reset & ((~inversion_found & sProcess & end_of_array) | (sYes & ~go));
-	wire sNo_next = ~reset & ((inversion_found & ~end_of_array & sProcess ) | (sNo & ~go));
-
-
-	wire sGarbage_next= reset | (sGarbage & ~go);
-	wire sStart_next = ~reset & go & (sStart | sGarbage | sYes | sNo);
-
-	dffe fsGarbage(sGarbage, sGarbage_next, clock, 1'b1 , 1'b0);
-
-	dffe fsForward(sForward, sForward_next, clock, 1'b1 , 1'b0);
-	dffe fsYes(sYes, sYes_next, clock, 1'b1 , 1'b0);
-	dffe fsNo(sNo, sNo_next, clock, 1'b1 , 1'b0);
-
-	dffe fsStart(sStart, sGarbage_next, clock, 1'b1 , 1'b0);
-	dffe fsProcess(sProcess, sProcess_next, clock, 1'b1 , 1'b0);
+	assign sorted = sort;
+	assign done = (notSort | sort);
+	assign load_input = doNothing;
+	assign load_index = (doNothing | check);
+	assign select_index = (doNothing | check);
 
 
-	assign done = sNo | sYes;
-	assign load_index = sForward | sStart;
-	assign select_index = sForward;
-	assign sorted = sYes;
-	assign load_input = sStart;
 
+	 
 endmodule
