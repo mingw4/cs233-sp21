478a8bb7d2d92823b4df46eb901640bd8566af10
mingw4 <mingw4@illinois.edu>
2021-02-14 18:27:21 -0600

    All over again. Last version a Complete Error

--- Lab3/arraySortCheck_control.v
+++ Lab3/arraySortCheck_control.v
@@ -4,40 +4,31 @@ module arraySortCheck_control(sorted, done, load_input, load_index, select_index
 	input go, inversion_found, end_of_array, zero_length_array;
 	input clock, reset;
 
-	wire sGarbage, sStart, s1, s2, s3, s4, sSortedDone, sunSortedDone, s0SortedDone, s0unSortedDone;
+	wire sGarbage, sStart, sProcess, sForward, sYes, sNo;
 
-	wire sGarbage_next = reset | (sGarbage & ~go) | (sStart & zero_length_array);
-	wire sStart_next = (sGarbage & go) & ~reset;
-	
-	wire s1_next = ~reset & sStart & ~zero_length_array & ~end_of_array & ~inversion_found;
-	wire s2_next = ~reset & s1 & ~zero_length_array & ~end_of_array & ~inversion_found;
-	wire s3_next = ~reset & s2 & ~zero_length_array & ~end_of_array & ~inversion_found;
-	wire s4_next = ~reset & s3 & ~zero_length_array & ~end_of_array & ~inversion_found;
+	wire sProcess_next = ~reset & (sForward | (sStart & ~go));
+	wire sForward_next = ~reset & ~end_of_array & ~inversion_found & sProcess;
 
-	wire s0SortedDone_next = ~reset & sStart & ~zero_length_array & end_of_array & ~inversion_found;
-	wire s0unSortedDone_next = ~reset & sStart & ~zero_length_array & end_of_array & inversion_found;
-	wire sSortedDone_next = ~reset & (s1 | s2 | s3 | s4) & ~zero_length_array & end_of_array & ~inversion_found;
-	wire sunSortedDone_next = ~reset & (s1 | s2 | s3 | s4) & ~zero_length_array & end_of_array & inversion_found;
-
-	dffe fsGarbage(sGarbage, sGarbage_next, clock, 1'b1, 1'b0);
-	dffe fsStart(sStart, sStart_next, clock, 1'b1, 1'b0);
-
-	dffe fs1(s1, s1_next, clock, 1'b1, 1'b0);
-	dffe fs2(s2, s2_next, clock, 1'b1, 1'b0);
-	dffe fs3(s3, s3_next, clock, 1'b1, 1'b0);
-	dffe fs4(s4, s4_next, clock, 1'b1, 1'b0);
-
-	dffe fs0SortedDone(s0unSortedDone, s0unSortedDone_next, clock, 1'b1, 1'b0);
-	dffe fs0unSortedDone(s0unSortedDone, s0unSortedDone_next, clock, 1'b1, 1'b0);
-	dffe fsSortedDone(sSortedDone, sSortedDone_next, clock, 1'b1, 1'b0);
-	dffe fsunSortedDone(sunSortedDone, sunSortedDone_next, clock, 1'b1, 1'b0);
-
-	assign sorted = sGarbage | sSortedDone | s0SortedDone;
-	assign done = sGarbage | s0SortedDone | s0unSortedDone | sSortedDone | sunSortedDone;
-	assign load_input = sStart;
-	assign load_index = ~sGarbage;
-	assign select_index = s1 | s2 | s3 | s4 | sSortedDone | sunSortedDone;
+	wire sYes_next = ~reset & ((~inversion_found & sProcess & end_of_array ) | (sSorted & ~go));
+	wire sUnsorted_next = ~reset & ((inversion_found & ~end_of_array & sProess ) | (sNo & ~go));
 
 
+	wire sGarbage_next= reset | (sGarbage & ~go);
+	wire sStart_next = ~reset & go & (sStart | sGarbage | sYes | sNo);
+
+	dffe fsGarbage(sGarbage, sGarbage_next, clock, 1'b1 , 1'b0);
+
+	dffe fsIncrement(sForward, sForward_next, clock, 1'b1 , 1'b0);
+	dffe fsSorted(sYes, sYes_next, clock, 1'b1 , 1'b0);
+	dffe fsUnsorted(sNo, sNo_next, clock, 1'b1 , 1'b0);
+
+	dffe fsStart(sStart, sGarbage_next, clock, 1'b1 , 1'b0);
+	dffe fsWhile(sProcess, sProcess_next, clock, 1'b1 , 1'b0);
+
+
+	assign done = sNo | sYes;
+	assign load_index = sForward | sStart;
+	assign select_index = sForward;
+	assign sorted = sYes;
 
 endmodule
