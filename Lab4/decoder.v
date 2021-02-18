87d99aebd57ba5eb92179ff8c3dd37634604ab25
mingw4 <mingw4@illinois.edu>
2021-02-18 13:07:16 -0600

    GROUP MERGE

--- Lab4/decoder.v
+++ Lab4/decoder.v
@@ -15,4 +15,26 @@ module mips_decode(rd_src, writeenable, alu_src2, alu_op, except, opcode, funct)
     output [2:0] alu_op;
     input  [5:0] opcode, funct;
 
+
+    wire add = ((opcode == 6'b000000) & (funct == 6'b100000));
+    wire sub = ((opcode == 6'b000000) & (funct == 6'b100010));
+    wire and_ = ((opcode == 6'b000000) & (funct == 6'b100100));
+    wire or_ = ((opcode == 6'b000000) & (funct == 6'b100101));
+    wire nor_ = ((opcode == 6'b000000) & (funct == 6'b100111));
+    wire xor_ = ((opcode == 6'b000000) & (funct == 6'b100110));
+    wire addi = (opcode == 6'b001000);
+    wire andi = (opcode == 6'b001100);
+    wire ori = (opcode == 6'b001101);
+    wire xori = (opcode == 6'b001110);
+
+    assign rd_src = (addi | andi | ori | xori);
+    assign writeenable = (add | sub | and_ | or_ | nor_ | xor_ | addi | andi | ori | xori);
+    assign except = ~writeenable;
+    assign alu_src2[0] = addi;
+    assign alu_src2[1] = (andi | ori | xori);
+    assign alu_op[0] = (sub | or_ | xor_ | ori | xori);
+    assign alu_op[1] = (add | sub | nor_ | xor_ | addi | xori);
+    assign alu_op[2] = (and_ | or_ | nor_ | xor_ | andi | ori | xori);
+
+    
 endmodule // mips_decode
