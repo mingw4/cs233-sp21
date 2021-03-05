1d4bb729a884b6f56b21be05e6e17d8bf0f1ba47
mingw4 <mingw4@illinois.edu>
2021-03-05 12:15:09 -0600

    FIRST

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -37,56 +37,95 @@
 
 .globl craftable_recipes
 craftable_recipes:
-li $s0, 5               #NUM_ITEMS
-li $s1, 10              #NUM_RECIPES
-li $s2, 0x7fffffff   
+sub $sp, $sp, 36
+sw $ra, 0($sp)
+sw $s0, 4($sp)
+sw $s1, 8($sp)
+sw $s2, 12($sp)
+sw $s3, 16($sp)
+sw $s4, 20($sp)
+sw $s5, 24($sp)
+sw $s6, 28($sp)
+sw $s7, 32($sp)
 
-li $t0, 0               #recipe_idx
-coe_FOR1:
-        bge $t0, $s0, coe_END1
-        add $t1, $t0, $t0
-        add $t1, $t1, $t1
-        add $t1, $t1, $a2
-        sw $s2, 0($t1)  #times_craftable[recipe_idx]
-        li $t2, 0       #assigned 0
-        li $t3,0        #item_idx
+move $s0, $a0                   #s0 = inventory[]
+move $s1, $a1                   #s1 = recipes[][]
+move $s2, $a2                   #s2 = times_craftable[]
 
-        coe_FOR2:
-                bge $t3, $s1, coe_END2
-                mult $s0, $t0
-                mflo $t4
-                add $t4, $t4, $t3
-                add $t4, $t4, $t4
-                add $t4, $t4, $t4
-                add $t4, $t4, $a1
-                lw $t4, 0($t4)          #recipes[recipe_idx][item_idx]
-                ble $t4, $zero, coe_END2
-                add $t5, $t3, $t3
-                add $t5, $t5, $t5
-                add $t5, $t5, $a0
-                lw $t6, 0($t5)          #inventory[item_idx]
-                div $t6, $t6, $t4       #times_item_req
-                lw $t7, 0($t1)          #times_craftable[recipe_idx]
-                bge $t6, $t7, INCREMENT2
-                sw $t6, 0($t1)
-                li $t2, 1
-        INCREMENT2:
-                addi $t3, $t3, 1
-                j coe_FOR2
-        coe_END2:
-                bne $t2, $zero, INCREMENT1
-                sw $zero, 0($t1)
-INCREMENT1:
-        addi $t0, $t0, 1
-        j coe_FOR1
-coe_END1:
+li $s3, 5                       #s3 = NUM_ITEMS num of col
+li $s4, 10                      #s4 = NUM_RECIPES 
+li $s5, 0x7fffffff              #s5 = Largest positive number
+
+
+li $t0, 0                       #t0 = recipe_idx
+
+FOR_LOOP1:
+        bge $t0, $s4, FOR_END1      #recipe_idx < NUM_RECIPES
+        mul $t1, $t0, 4         #recipe_idx * 4
+        add $t1, $t1, $s2       #address_craftable = recipe_idx * 4 + craftable[]
+        sw $s5, 0($t1)          #times_craftable[recipe_idx] = 0x7fffffff
+        li $t2, 0               #t2 = assigned = 0
+
+        li $t3, 0               #t3 = item_idx = 0
+
+FOR_LOOP2:
+        bge $t3, $s3, INCR1     #item_idx < NUM_ITEMS
+        mul $t4, $t0, $s3       #t4 = recipe_idx * num of col
+        add $t4, $t4, $t3       #t4 = recipe_idx * num of col + col
+        mul $t4, $t4, 4         #t4 = 4 * (recipe_idx * num of col + col)
+        add $t4, $t4, $s1       #addr_of_recipes[row][col] = addr_of_recipes[][] + 4 * (recipe_idx * num of col + col)
+        lw $s6, 0($t4)          #s6 = addr_of_recipes[row][col]
+
+IF1:
+        ble $s6, $zero, ENDIF1
+        mul $t5, $t3, 4         #t5 = item_idx * 4
+        add $t5, $t5, $s0       #t5 = addr_of_inventory[item_idx] = inventory[] + 4 * item_idx
+        lw $t5, 0($t5)          #t5 = inventory[item_idx]
+        div $t6, $t5, $s6       #t6 = times_item_req = inventory[item_idx]/recipes[recipe_idx][item_idx]
+
+        mul $t7, $t0, 4         #t7 = recipe_idx * 4
+        add $t7, $t7, $a2       #t7 = addr_of_times_craftable[recipe_idx]
+        lw $t8, 0($t7)          #t8 = times_craftable[recipe_idx]
+
+
+IF11:
+        bge $t6, $t8, ENDIF1    #times_item_req < times_craftable[recipe_idx]
+        sw $t6, 0($t7)          #times_craftable[recipe_idx] = times_item_req
+        li $t2, 1               #t2 = assigned = 1
+
+ENDIF1:
+        bne $t2, $zero, INCR2
+        sw $zero, 0($t7)
+
+
+INCR2:
+        addi $t3, $t3, 1        #t3 = t3 + 1 = item_idx + 1
+        j FOR_LOOP2
+
+INCR1:
+        addi $t0, $t0, 1        #t0 = t0 + 1 = recipe_idx + 1
+        j FOR_LOOP1
+
+FOR_END1:
+
 
         
 
 
-        
 
 
 
 
-        jr      $ra
+
+lw $ra, 0($sp)
+lw $s0, 4($sp)
+lw $s1, 8($sp)
+lw $s2, 12($sp)
+lw $s3, 16($sp)
+lw $s4, 20($sp)
+lw $s5, 24($sp)
+lw $s6, 28($sp)
+lw $s7, 32($sp)
+
+add $sp, $sp, 36
+jr $ra
