c577ea239f40beb5228917c2e80ba59e292a1655
mingw4 <mingw4@illinois.edu>
2021-03-14 04:02:57 -0500

    steps I

--- Lab7/count_disjoint_regions_step.s
+++ Lab7/count_disjoint_regions_step.s
@@ -29,4 +29,69 @@
 .globl count_disjoint_regions_step
 count_disjoint_regions_step:
         # Your code goes here :)
-        jr      $ra
+sub $sp, $sp, 32
+sw $ra, 0($sp)
+sw $s0, 4($sp)
+sw $s1, 8($sp)
+sw $s2, 12($sp)
+sw $s3, 16($sp)
+sw $s4, 20($sp)
+sw $s5, 24($sp)
+sw $s6, 28($sp)
+
+
+move $s0, $zero                         #s0 = region_count = 0
+move $s1, $zero                         #s1 = row = 0
+move $s2, $zero                         #s2 = col = 0
+lw $s3, 0($a1)                          #s3 = canvas->height
+lw $s4, 4($a1)                          #s4 = canvas->width
+move $s5, $a0                           #s5 = a0 = marker
+move $s6, $a1                           #s6 = a1 = addr_canvas
+
+FOR1:
+        bge $s1, $s3, END               #s1 = row >= canvas->height = s3
+FOR2:
+        bge $s2, $s4, INCR1             #s2 = col >= canvas->width = s4
+        lw $t0, 12($a1)                 #t0 = canvas->canvas
+        mul $t1, $s1, 4                 #t1 = 4 * s1 = 4 * row
+        add $t1, $t1, $t0               #t1 = addr_canvas[row]
+        lw $t1, 0($t1)                  #t1 = canvas[row]
+        add $t1, $t1, $s2               #t1 = addr_canvas[row][col]
+        lb $t2, 0($t1)                  #t2 = curr_char = canvas[row][rol]
+
+IF:
+        lb $t3, 8($a1)                  #t3 = canvas->pattern
+        beq $t2, $t3, INCR2             #curr_char == canvas->pattern
+        beq $t2, $a0, INCR2             #curr_char == marker
+
+        addi $s0, $s0, 1
+        move $a0, $s1
+        move $a1, $s2
+        move $a2, $s5
+        move $a3, $s6
+        jal flood_fill
+
+INCR2:
+        addi $s2, $s2, 1
+INCR1:
+        addi $s1, $s1, 1
+END:
+        move $v0, $s0
+
+
+
+lw $ra, 0($sp)
+lw $s0, 4($sp)
+lw $s1, 8($sp)
+lw $s2, 12($sp)
+lw $s3, 16($sp)
+lw $s4, 20($sp)
+lw $s5, 24($sp)
+lw $s6, 28($sp)
+
+add $sp, $sp, 32
+
+
+
+
+jr      $ra
