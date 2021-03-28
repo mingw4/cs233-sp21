3b19749659ee91cef736f12990f402bfa785bc11
mingw4 <mingw4@illinois.edu>
2021-03-28 17:55:17 -0500

    Done

--- Lab9/part2.s
+++ Lab9/part2.s
@@ -43,7 +43,9 @@ GET_INVENTORY           = 0xffff2034
 ### Puzzle
 puzzle:     .byte 0:400
 solution:   .byte 0:256
+sol_t:      .word 12 solution
 puzzle_received: .word 0
+
 #### Puzzle
 
 scanner_result: .byte 0 0 0
@@ -73,12 +75,65 @@ main:
     sw $t1, ANGLE_CONTROL
     li $t2, 0
     sw $t2, VELOCITY
+    jal PUZZLE_PART
+    jal PART_1
         
     # YOUR CODE GOES HERE!!!!!!
 
-    lw $t1, BOT_X                               #t1 = BOT_X
-    lw $t2, BOT_Y                               #t2 = BOT_Y
+PUZZLE_PART:
+    sub $sp, $sp, 28
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    sw $s2, 12($sp)
+    sw $s3, 16($sp)
+    sw $s4, 20($sp)
+    sw $s5, 24($sp)
+
+
+move $s0, $zero                             #s0 = i = 0
+FOR:
+    bge $s0, 2, PUZZLE_END                  #i < 2
+    sw $0, puzzle_received
+    la $s2, puzzle                          #s2 = addr_puzzle
+    sw $s2, REQUEST_PUZZLE                  #*REQUEST_PUZZLE = &puzzle
+
+    WHILE:
+        lw $t0, puzzle_received                      #t0 = puzzle_received
+        bne $t0, $0, WHILE_END              #puzzle_received == 0
+        j WHILE
     
+    WHILE_END:
+        addi $a0, $s2, 16                   #a0 = *line
+        move $a1, $s2                       #a1 = *canvas
+        la $a2, sol_t                    #a2 = *solution
+        jal count_disjoint_regions
+
+        la $s3, solution                    #s3 = addr_solution
+        sw $s3, SUBMIT_SOLUTION             #*SUBMIT_SOLUTION = &solution
+        addi $s0, $s0, 1                    #i++
+        j FOR
+
+PUZZLE_END:
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    lw $s2, 12($sp)
+    lw $s3, 16($sp)
+    lw $s4, 20($sp)
+    lw $s5, 24($sp)
+
+    add $sp, $sp, 28
+    jr $ra
+    
+
+
+
+
+PART_1:
+
+lw $t1, BOT_X                               #t1 = BOT_X
+lw $t2, BOT_Y                               #t2 = BOT_Y
 
 SOUTH_TO_WOOD:
     bge $t2, 284, EAST_TO_WOOD
@@ -165,39 +220,247 @@ COLLECT_STONE:
 
     li $t4, 0x00002503                          #t4 = STONE = 37, 3
     sw $t4, BREAK_BLOCK                         #break the STONE block 
-
-    li $t0, 0                                   #t0 = i = 0
-    FOR:
-        bge $t0, 2, DO_CRAFT
-        sw $0, puzzle_received                  #puzzle_received = 0
-        la $t1, puzzle                          #t1 = &puzzle
-        sw $t1, REQUEST_PUZZLE                  #*REQUEST_PUZZLE = &puzzle
-
-        WHILE:
-            lw $t2, puzzle_received             #t2 = puzzle_received
-            bne $t2, 0, WHILE_END
-            j WHILE
-        
-        WHILE_END:
-            la $t3, solution                    #t3 = &solution
-            sw $t3, SUBMIT_SOLUTION             #*SUBMIT_SOLUTION = &solution
-            addi $t0, $t0, 1                    #i++
-            j FOR
+    j DO_CRAFT
 
 DO_CRAFT:
     li $t4, 7
     sw $t4, CRAFT
-
-
-
-
-
-
-
     
+
+
 loop: # Once done, enter an infinite loop so that your bot can be graded by QtSpimbot once 10,000,000 cycles have elapsed
     j main
+
+
+
+
+
+
+
+
+
     
+
+
+count_disjoint_regions:
+        # Your code goes here :)
+        sub     $sp, $sp, 20
+        sw      $ra, 0($sp)
+        sw      $s0, 4($sp)
+        sw      $s1, 8($sp)
+        sw      $s2, 12($sp)
+        sw      $s3, 16($sp)
+
+        move    $s0, $a0                # line
+        move    $s1, $a1                # canvas
+        move    $s2, $a2                # solution
+
+        li      $s3, 0                  # unsigned int i = 0;
+loop1:
+        lw	$t0, 0($s0)		# $t0 = lines->num_lines
+        bge     $s3, $t0, end1           # i < lines->num_lines : fallthrough
+        
+        #lines->coords[0][i];
+        lw	$t1, 4($s0)		# $t1 = &(lines->coords[0][0])
+        lw	$t2, 8($s0)		# $t2 = &(lines->coords[1][0])
+
+        mul     $t3, $s3, 4             # i * sizeof(int*)
+        add     $t1, $t3, $t1           # $t1 = &(lines->coords[0][i])
+        add     $t2, $t3, $t2           # $t2 = &(lines->coords[1][i])
+
+        lw      $a0, 0($t1)             # $a0 = lines->coords[0][i] = start_pos
+        lw      $a1, 0($t2)             # $a1 = lines->coords[0][i] = end_pos
+        move    $a2, $s1                # $a2 = canvas
+        jal     draw_line               # draw_line(start_pos, end_pos, canvas);
+
+        li      $a0, 65                 # Immediate value A
+        rem     $t1, $s3, 2             # i % 2
+        add     $a0, $a0, $t1           # 'A' or 'B'
+        move    $a1, $s1
+        jal     count_disjoint_regions_step  # count_disjoint_regions_step('A' + (i % 2), canvas);
+        # $v0 = count_disjoint_regions_step('A' + (i % 2), canvas);
+
+        lw      $t0, 4($s2)             # &counts = &counts[0]
+        mul     $t1, $s3, 4             #  i * sizeof(unsigned int)
+        add     $t0, $t1, $t0           # *counts[i]
+        sw      $v0, 0($t0)
+
+##         // Update the solution struct. Memory for counts is preallocated.
+##         solution->counts[i] = count;
+
+
+        add     $s3, $s3, 1             # i++
+        j       loop1
+end1:
+        lw      $ra, 0($sp)
+        lw      $s0, 4($sp)
+        lw      $s1, 8($sp)
+        lw      $s2, 12($sp)
+        lw      $s3, 16($sp)
+        add     $sp, $sp, 20
+        jr      $ra
+
+
+draw_line:
+        lw      $t0, 4($a2)     # t0 = width = canvas->width
+        li      $t1, 1          # t1 = step_size = 1
+        sub     $t2, $a1, $a0   # t2 = end_pos - start_pos
+        blt     $t2, $t0, cont
+        move    $t1, $t0        # step_size = width;
+cont:
+        move    $t3, $a0        # t3 = pos = start_pos
+        add     $t4, $a1, $t1   # t4 = end_pos + step_size
+        lw      $t5, 12($a2)    # t5 = &canvas->canvas
+        lbu     $t6, 8($a2)     # t6 = canvas->pattern
+for_loop:
+        beq     $t3, $t4, end_for
+        div     $t3, $t0        #
+        mfhi    $t7             # t7 = pos % width
+        mflo    $t8             # t8 = pos / width
+        mul     $t9, $t8, 4		# t9 = pos/width*4
+        add     $t9, $t9, $t5   # t9 = &canvas->canvas[pos / width]
+        lw      $t9, 0($t9)     # t9 = canvas->canvas[pos / width]
+        add     $t9, $t9, $t7
+        sb      $t6, 0($t9)     # canvas->canvas[pos / width][pos % width] = canvas->pattern
+        add     $t3, $t3, $t1   # pos += step_size
+        j       for_loop
+        
+end_for:
+        jr      $ra
+
+
+count_disjoint_regions_step:
+        # Your code goes here :)
+        sub	$sp, $sp, 24
+	sw	$ra, 0 ($sp)
+	sw	$s0, 4 ($sp)                    # marker
+        sw      $s1, 8 ($sp)                    # canvas
+        sw      $s2, 12($sp)                    # region_count
+        sw      $s3, 16($sp)                    # row
+        sw      $s4, 20($sp)                    # col
+	
+        move    $s0, $a0
+        move    $s1, $a1
+	li	$s2, 0			        # unsigned int region_count = 0;
+        
+        
+        li      $s3, 0                          # row = 0
+outer_loop:                                     # for (unsigned int row = 0; row < canvas->height; row++) {
+        lw      $t0, 0($s1)                     # canvas->height
+        bge     $s3, $t0, end_outer_loop        # row < canvas->height : fallthrough
+
+        li      $s4, 0                          # col = 0
+inner_loop:                                     # for (unsigned int col = 0; col < canvas->width; col++) {
+        lw      $t0, 4($s1)                     # canvas->width
+        bge     $s4, $t0, end_inner_loop        # col < canvas->width : fallthrough
+
+        
+        # unsigned char curr_char = canvas->canvas[row][col];
+        lw      $t1, 12($s1)                    # &(canvas->canvas)
+        mul     $t2, $s3, 4                     # $t2 = row * 4
+        add     $t2, $t2, $t1                   # $t2 = canvas->canvas + row * sizeof(char*) = canvas[row]
+        lw	$t1, 0($t2)		        # $t1 = &char = char* = & canvas[row][0]
+        add	$t1, $s4, $t1           	# $t1 = &canvas[row][col]
+        lb	$t1, 0($t1)		        # $t1 = canvas[row][col] = curr_char
+
+        lb      $t2, 8($s1)                     # $t2 = canvas->pattern 
+
+        # temps:        $t1 = curr_char         $t2 = canvas->pattern
+
+        # if (curr_char != canvas->pattern && curr_char != marker) {
+        beq     $t1, $t2, endif                 # if (curr_char != canvas->pattern) fall
+        beq	$t1, $s0, endif                 # if (curr_char != marker)          fall
+        
+        add     $s2, $s2, 1                     # region_count ++;
+        move    $a0, $s3                        # (row,
+        move    $a1, $s4                        #  col,
+        move    $a2, $s0                        #  marker,
+        move    $a3, $s1                        #  canvas);
+        jal     flood_fill                      # flood_fill(row, col, marker, canvas);
+ 
+endif:
+
+
+        add     $s4, $s4, 1                     # col++
+        j       inner_loop                      # loop again
+end_inner_loop:
+
+
+        add     $s3, $s3, 1                     # row++
+        j       outer_loop                      # loop again
+end_outer_loop:
+
+	move	$v0, $s2		# Copy return val
+	lw	$ra, 0($sp)
+	lw	$s0, 4 ($sp)                    # marker
+        lw      $s1, 8 ($sp)                    # canvas
+        lw      $s2, 12($sp)                    # region_count
+        lw      $s3, 16($sp)                    # row
+        lw      $s4, 20($sp)                    # col
+
+	add	$sp, $sp, 24
+	jr      $ra
+
+
+flood_fill:
+        # Your code goes here :)
+	blt	$a0, $zero, end		# row < 0 
+	blt	$a1, $zero, end		# col < 0
+	lw	$t0, 0($a3)		# $t0 = canvas->height
+	bge	$a0, $t0, end		# row >= canvas->height
+	lw	$t0, 4($a3)		# $t0 = canvas->width
+	bge	$a1, $t0, end		# col >= canvas->width
+	j 	recur			# NONE TRUE
+
+recur:
+	# Find curr
+	lw	$t0, 12($a3)		# canvas->canvas
+	mul	$t1, $a0, 4		# row * sizeof(char*)
+	add	$t1, $t1, $t0		# $t1 = canvas->canvas + row * sizeof(char*) = canvas[row]
+	lw	$t2, 0($t1)		# $t2 = &char = char* = & canvas[row][0]
+	add	$t2, $a1, $t2		# $t2 = &canvas[row][col]
+	lb	$t3, 0($t2)		# $t3 = curr
+	
+	lb	$t4, 8($a3)		# $t4 = canvas->pattern
+	
+	beq	$t3, $t4, end		# curr == canvas->pattern : break 
+	beq	$t3, $a2, end		# curr == marker          : break
+	
+	#FLOODFILL
+	sb	$a2, ($t2) 
+	
+	# Save depenedecies
+	sub	$sp, $sp, 12
+	sw	$ra, 0($sp)
+	sw	$s0, 4($sp)
+	sw	$s1, 8($sp)
+	move	$s0, $a0
+	move	$s1, $a1
+	
+	sub	$a0, $s0, 1
+	move	$a1, $s1
+	jal	flood_fill
+
+	move	$a0, $s0
+	add	$a1, $s1, 1
+	jal	flood_fill
+
+	add	$a0, $s0, 1
+	move	$a1, $s1
+	jal	flood_fill
+
+	move	$a0, $s0
+	sub	$a1, $s1, 1
+	jal	flood_fill
+	
+	# Restore VARS
+	lw	$ra, 0($sp)
+	lw	$s0, 4($sp)
+	lw	$s1, 8($sp)
+	add	$sp, $sp, 12
+end:
+	jr 	$ra
+
 
 .kdata
 chunkIH:    .space 40
@@ -271,16 +534,14 @@ timer_interrupt:
 request_puzzle_interrupt:
     sw      $0, REQUEST_PUZZLE_ACK
     #Fill in your puzzle interrupt code here
-    li $t5, 1
-    sw $t5, REQUEST_PUZZLE_ACK
-    sw $t5, puzzle_received
+    li $t0, 1
+    sw $t0, REQUEST_PUZZLE_ACK
+    sw $t0, puzzle_received
     j       interrupt_dispatch
 
 respawn_interrupt:
     sw      $0, RESPAWN_ACK
     #Fill in your respawn handler code here
-
-
     j       interrupt_dispatch
 
 non_intrpt:                         # was some non-interrupt
@@ -313,3 +574,4 @@ done:
     move    $at, $k1        # Restore $at
 .set at
     eret
+
